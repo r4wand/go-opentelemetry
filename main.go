95613b450b4fedf6fed7f6329afa31d8ddc1bc2b
Rawand Rebwar <26229485+r4wand@users.noreply.github.com>
2023-09-11 01:50:02 +0300

    feat: added sample application and trade export

--- main.go
+++ main.go
@@ -1,5 +1,189 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
 
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"google.golang.org/grpc/credentials"
+
+	"go.opentelemetry.io/otel/trace"
+
+	sdkTrace "go.opentelemetry.io/otel/sdk/trace"
+
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+)
+
+var (
+	serviceName  = "my_app"
+	collectorURL = "localhost:4317"
+	insecure     = true
+)
+
+func initTracer() func(context.Context) error {
+
+	secureOption := otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
+	if insecure {
+		secureOption = otlptracegrpc.WithInsecure()
+	}
+
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracegrpc.NewClient(
+			secureOption,
+			otlptracegrpc.WithEndpoint(collectorURL),
+		),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	resources, err := resource.New(
+		context.Background(),
+		resource.WithAttributes(
+			attribute.String("service.name", serviceName),
+			attribute.String("library.language", "go"),
+		),
+	)
+	if err != nil {
+		log.Printf("Could not set resources: ", err)
+	}
+
+	otel.SetTracerProvider(
+		sdkTrace.NewTracerProvider(
+			sdkTrace.WithSampler(sdkTrace.AlwaysSample()),
+			sdkTrace.WithBatcher(exporter),
+			sdkTrace.WithResource(resources),
+		),
+	)
+	return exporter.Shutdown
+}
+
+var name = "fib"
+
+func main() {
+	l := log.New(os.Stdout, "", 0)
+
+	cleanup := initTracer()
+	defer cleanup(context.Background())
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+
+	errCh := make(chan error)
+	app := NewApp(os.Stdin, l)
+	go func() {
+		errCh <- app.Run(context.Background())
+	}()
+
+	select {
+	case <-sigCh:
+		l.Println("\ngoodbye")
+		return
+	case err := <-errCh:
+		if err != nil {
+			l.Fatal(err)
+		}
+	}
+}
+
+// App is a Fibonacci computation application.
+type App struct {
+	r io.Reader
+	l *log.Logger
+}
+
+// NewApp returns a new App.
+func NewApp(r io.Reader, l *log.Logger) *App {
+	return &App{r: r, l: l}
+}
+
+// Run starts polling users for Fibonacci number requests and writes results.
+func (a *App) Run(ctx context.Context) error {
+	for {
+
+		newCtx, span := otel.Tracer(name).Start(ctx, "Run")
+
+		n, err := a.Poll(newCtx)
+		if err != nil {
+			span.End()
+			return err
+		}
+
+		a.Write(ctx, n)
+		span.End()
+	}
+}
+
+// Poll asks a user for input and returns the request.
+func (a *App) Poll(ctx context.Context) (uint, error) {
+	_, span := otel.Tracer(name).Start(ctx, "Poll")
+	defer span.End()
+
+	a.l.Print("What Fibonacci number would you like to know: ")
+
+	var n uint
+	_, err := fmt.Fscanf(a.r, "%d\n", &n)
+	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+		return 0, err
+	}
+
+	// Store n as a string to not overflow an int64.
+	nStr := strconv.FormatUint(uint64(n), 10)
+	span.SetAttributes(attribute.String("request.n", nStr))
+
+	return n, nil
+}
+
+// Write writes the n-th Fibonacci number back to the user.
+func (a *App) Write(ctx context.Context, n uint) {
+	var span trace.Span
+	ctx, span = otel.Tracer(name).Start(ctx, "Write")
+	defer span.End()
+
+	f, err := func(ctx context.Context) (uint64, error) {
+		_, span := otel.Tracer(name).Start(ctx, "Fibonacci")
+		defer span.End()
+		f, err := Fibonacci(n)
+		if err != nil {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
+		}
+		return f, err
+	}(ctx)
+
+	if err != nil {
+		a.l.Printf("Fibonacci(%d): %v\n", n, err)
+	} else {
+		a.l.Printf("Fibonacci(%d) = %d\n", n, f)
+	}
+}
+
+// Fibonacci returns the n-th Fibonacci number.
+func Fibonacci(n uint) (uint64, error) {
+	if n <= 1 {
+		return uint64(n), nil
+	}
+
+	if n > 93 {
+		return 0, fmt.Errorf("unsupported Fibonacci number %d: too large", n)
+	}
+
+	var n2, n1 uint64 = 0, 1
+	for i := uint(2); i < n; i++ {
+		n2, n1 = n1, n1+n2
+	}
+
+	return n2 + n1, nil
 }
